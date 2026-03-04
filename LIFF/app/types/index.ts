2caf63d7a2cdd12fd72709adbbd78ef9e6e56310
Piyabordee <piyabordee.phongam@gmail.com>
2026-03-04 12:45:05 +0700

    feat: add ticket types

--- LIFF/app/types/index.ts
+++ LIFF/app/types/index.ts
@@ -10,3 +10,21 @@ export interface LiffContextType {
   error: Error | null
   initialized: boolean
 }
+
+export interface Ticket {
+  message_id: string
+  subject: string
+  status: 'pending' | 'assigned' | 'closed' | 'unsent'
+  category: string
+  sub_category: string
+  branch_name: string
+  created_date: string
+}
+
+export interface TicketsResponse {
+  kpi: {
+    total: number
+    closed: number
+  }
+  tickets: Ticket[]
+}
